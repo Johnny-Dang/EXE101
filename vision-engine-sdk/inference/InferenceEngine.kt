8778faebf9c35453ccf8875172dedc0748ae13a2
Vũ Trần Bình Minh <binhminhvutran@gmail.com>
2026-04-27 09:54:26 +0700

    Remove - All

--- vision-engine-sdk/inference/InferenceEngine.kt
+++
@@ -1,9 +0,0 @@
-package com.visionengine.inference
-
-import java.nio.ByteBuffer
-
-interface InferenceEngine {
-    suspend fun load(modelPath: String)
-    suspend fun infer(input: ByteBuffer): FloatArray
-    fun release()
-}
